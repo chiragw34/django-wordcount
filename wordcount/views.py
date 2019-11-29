29000d269515e848ed9a56a42e37b5575e3a8714
Chirag Wadhwa <chiragw34@gmail.com>
2019-11-29 23:27:46 +0530

    basic django website

---
+++ wordcount/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    print(fulltext)
+    wordlist = fulltext.split()
+
+    wordDict = {}
+    for word in wordlist:
+        if word in wordDict:
+            # increase
+            wordDict[word] += 1
+        else:
+            # add to dictionary
+            wordDict[word] = 1
+    sortedWords = sorted(wordDict.items(), key=operator.itemgetter(1), reverse = True)
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'sortedWords': sortedWords})
